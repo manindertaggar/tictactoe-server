73368038fe7c0a8e85f50b0d2df8cdbca89b543d
Maninder Taggar <manindersingh030@gmail.com>
2017-01-29 16:13:06 +0530

    rolled back to log

--- Log.php
+++ Log.php
@@ -1,5 +1,5 @@
 <?php
-class Logger
+class Log
 {
     public static function e($obj, $message)
     {
--- TicTacToeEngine.php
+++ TicTacToeEngine.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-require 'Log.php';
+
 require 'CredentialVerifier.php';
 require 'Log.php';
 require 'Output.php';
