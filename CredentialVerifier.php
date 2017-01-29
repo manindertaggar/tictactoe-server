01d83744d18c603de4be67c77727fee183117cdf
Maninder Taggar <manindersingh030@gmail.com>
2017-01-29 16:41:54 +0530

    CredentialVerfier done

--- CredentialVerifier.php
+++ CredentialVerifier.php
@@ -1,14 +1,41 @@
 <?php
+
 class CredentialVerifier
 {
-	private $conn;
+    private $conn;
+    private $output;
+
     public function __construct($conn)
     {
-        $this->conn = $conn;
+        $this->conn   = $conn;
+        $this->output = new Output();
     }
 
     public function verify($playerId, $token)
     {
-        return true;
+
+//        return true;
     }
-}
+
+    // private function getHashFor($data)
+    // {
+    //     $hash = password_hash($data, CRYPT_BLOWFISH);
+    //     return $hash;
+    // }
+
+    private function verifyCredentils($emailId, $password)
+    {
+
+        $sql    = "SELECT * FROM playersData WHERE `emailId` = '$emailId'";
+        $result = $this->conn->query($sql)->fetch_assoc();
+        if (!$result) {
+            Log::e($this, "verifyCredentils: database exception".mysqli_error($this->conn));
+            return false;
+        }
+        $hash = $result['password'];
+
+        if (password_verify($password, $hash)) {
+            return $result['playerId'];
+        }
+        return false;
+    }}
--- Cryptography.php
+++
@@ -1,5 +0,0 @@
-<?php
-function getHashFor($data){
-    $hash = password_hash($data, CRYPT_BLOWFISH);
-    return $hash;
-}
